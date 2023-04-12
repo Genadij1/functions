73d5cc00b6bab669d1d767e77c9762566a615e9d
Genacud <bgennadij265@icloud.com>
2023-04-12 20:41:30 +0300

    ex_1-done

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 def average(numbers):
     return sum(numbers) / len(numbers)
-
-numbers = int(input('enter numbers'))
+numbers = int(input('enter num1' )), int(input('num2:')),int(input('num3:')),int(input('num4:')),int(input('num5:'))
 print(average(numbers))
