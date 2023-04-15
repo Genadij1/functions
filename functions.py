99ca7e63fbd4af92a32e4e87b938dba634248ea3
Genacud <bgennadij265@icloud.com>
2023-04-15 19:19:28 +0300

    ex_3-done

--- functions.py
+++ functions.py
@@ -1,1 +1,10 @@
-
+import random
+list = [random.randint(1, 10) for i in range(10)]
+def get_unique_number(list):
+    unique = []
+    for i in list:
+        if i not in unique:
+            unique.append(i)
+    return unique
+print(list)
+print(get_unique_number(list))
