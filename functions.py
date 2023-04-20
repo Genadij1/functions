de96f1dbc2cad05e163f5588f139710777f13a9f
Genacud <bgennadij265@icloud.com>
2023-04-20 20:24:53 +0300

    ex_5-done

--- functions.py
+++ functions.py
@@ -3,8 +3,5 @@ def capitalize_words(string):
     return string.title()
 string = input('Enter a string: ')
 print (capitalize_words(string))
-text = input('Hi, how are you ?')
-def Hello():
-    text = input('Hi, how are you ?')
-    print (text)
+
 
