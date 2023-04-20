c6e90d9efa48dbfbb6c029814e0af4e6f4716451
Genacud <bgennadij265@icloud.com>
2023-04-20 19:29:49 +0300

    updata

--- functions.py
+++ functions.py
@@ -1,15 +1,7 @@
-import random
-def most_frequent(numbers):
-    count = {}
-    for num in numbers:
-        if num in count:
-            count[num] += 1
-        else:
-            count[num] = 1
-    max_count = max(count.values())
-    most_frequent_nums = [k for k, v in count.items() if v == max_count]
-    return min(most_frequent_nums)
-numbers = [random.randint(1, 100) for _ in range(100)]
-print(numbers)
-most_frequent(numbers)
-print(most_frequent(numbers))
+import string
+
+string = input('enter text: ')
+def string.charAt(0):
+print(string.charAt(string))
+
+
