779b08dbd2cc872f602ea8c3cc17f221e9658041
Genacud <bgennadij265@icloud.com>
2023-04-22 13:50:24 +0300

    ex_9-done

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
+string = (input("Введіть рядок: "))
+def get_first_and_last_three_chars(string):
+    if len(string) < 6:
+        return "Недостатньо символів"
+    else:
+        return string[:3] + string[-3:]
+print(get_first_and_last_three_chars(string))
 
